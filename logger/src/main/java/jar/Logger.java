bc939f876b89796e6f75ccc2c950339a4c297bb4
jensenb <jensenb@student.chalmers.se>
2020-11-29 17:37:43 +0100

    Add preliminary paho dependencies

---
+++ logger/src/main/java/jar/Logger.java
@@ -0,0 +1,18 @@
+package jar;
+
+import org.eclipse.paho.client.mqttv3.IMqttClient;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
+import org.eclipse.paho.client.mqttv3.MqttSecurityException;
+
+public class Logger 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
