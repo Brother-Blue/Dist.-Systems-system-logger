56db0782649d24147a0995ba60b2a4c40d66d7b5
jensenb <jensenb@student.chalmers.se>
2020-11-30 14:02:23 +0100

    Preliminary file logger with placeholder data

--- logger/src/main/java/jar/Logger.java
+++ logger/src/main/java/jar/Logger.java
@@ -1,53 +1,47 @@
 package jar;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import org.eclipse.paho.client.mqttv3.IMqttClient;
-import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
-// import org.eclipse.paho.client.mqttv3.MqttCallback;
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttException;
-// import org.eclipse.paho.client.mqttv3.MqttMessage;
-// import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
-import org.eclipse.paho.client.mqttv3.MqttSecurityException;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 public class Logger {
 
-    private final static ExecutorService THREAD_POOL = Executors.newSingleThreadExecutor();
-    private final IMqttClient middleware;
-
-    public Logger(String userId, String broker) throws MqttException {
-        middleware = new MqttClient(broker, userId);
-        middleware.connect();
+    @SuppressWarnings("unchecked")
+    public static void main( String[] args )
+    {
+        //First Employee
+        JSONObject employeeDetails = new JSONObject();
+        employeeDetails.put("firstName", "Lokesh");
+        employeeDetails.put("lastName", "Gupta");
+        employeeDetails.put("website", "howtodoinjava.com");
+         
+        JSONObject employeeObject = new JSONObject(); 
+        employeeObject.put("employee", employeeDetails);
+         
+        //Second Employee
+        JSONObject employeeDetails2 = new JSONObject();
+        employeeDetails2.put("firstName", "Brian");
+        employeeDetails2.put("lastName", "Schultz");
+        employeeDetails2.put("website", "example.com");
+         
+        JSONObject employeeObject2 = new JSONObject(); 
+        employeeObject2.put("employee", employeeDetails2);
+         
+        //Add employees to list
+        JSONArray employeeList = new JSONArray();
+        employeeList.add(employeeObject);
+        employeeList.add(employeeObject2);
+         
+        //Write JSON file
+        try (FileWriter file = new FileWriter("logger/src/main/java/jar/Log.json")) {
+ 
+            file.write(employeeList.toJSONString());
+            file.flush();
+            file.close();
+ 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
-    public static void main(String[] args) throws MqttException, InterruptedException {
-        Logger logger = new Logger("test-logger", "tcp://localhost:1883");
-        logger.subscribeToMessages("frosk");
-    }
-
-    private void subscribeToMessages(String sourceTopic) {
-		THREAD_POOL.submit(() -> {
-			try {
-				middleware.subscribe(sourceTopic);
-			} catch (MqttSecurityException e) {
-				e.printStackTrace();
-			} catch (MqttException e) {
-				e.printStackTrace();
-			}
-		});
-	}
-
-    // Seems important - don't touch 
-	public void connectionLost(Throwable throwable) {
-		System.out.println("Connection lost!");
-		try {
-			middleware.disconnect();
-			middleware.close();
-		} catch (MqttException e) {
-			e.printStackTrace();
-		}
-		// Try to reestablish? Plan B?
-	}
 }
